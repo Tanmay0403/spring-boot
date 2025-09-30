0c4debf9c238e564da6c72811517cb9664f8457f
Tanmay Chachad <tanmaychachad@gmail.com>
2025-09-30 08:47:37 +0530

    update

---
+++ demo/src/main/java/com/example/demo/Config.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        var user1 = User.withUsername("admin")
+                .password(encoder.encode("password"))
+                .roles("USER")
+                .build();
+
+       
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // disable CSRF for testing (don’t do this in production)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/employee/**").authenticated() // secure your employee API
+                .anyRequest().permitAll()
+            )
+            .httpBasic();
+        return http.build();
+    }
+}
